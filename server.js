166eda2b1e14cf35025ce0bdfa91180e596e039a
sunil jangid <132828535+snljgd@users.noreply.github.com>
2025-08-20 11:32:03 +0530

    new code

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ app.get("/",(req,res)=>{
     res.send("hello node class augest node js" )
 })
 
-
+app.get("/signup",(req,res)=>{
+    res.send("hello node class augest node js" )
+})
 app.get("/login",(req,res)=>{
     res.send("login page")
 })
