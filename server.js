a69deb39993bbc9b68bbc7592bd7350e4f344c36
sunil jangid <132828535+snljgd@users.noreply.github.com>
2025-08-20 11:30:56 +0530

    node new

---
+++ server.js
@@ -0,0 +1,19 @@
+const express =require('express');
+const app = express();
+
+const port = 5000;
+app.get("/",(req,res)=>{
+    res.send("hello node class augest node js" )
+})
+
+
+app.get("/login",(req,res)=>{
+    res.send("login page")
+})
+
+
+
+app.listen(po,()=>{
+    console.log("server start")
+})
+
