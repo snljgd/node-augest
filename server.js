b8de8b85c7f5ad8129e291b7da7d75ab17365fc3
sunil jangid <132828535+snljgd@users.noreply.github.com>
2025-08-22 06:47:27 +0530

    change

--- server.js
+++ server.js
@@ -4,16 +4,84 @@ const app = express();
 const port = 5000;
 
 
+// app.get("/",(req,res)=>{
+//     res.send("hello node class augest node js" )
+// })
+
+// app.get("/signup",(req,res)=>{
+//     res.send("hello node class augest node js" )
+// })
+// app.get("/login",(req,res)=>{
+//     res.send("login page")
+// })
+
+app.set("view engine","ejs");
+
+
 app.get("/",(req,res)=>{
-    res.send("hello node class augest node js" )
+    res.send("<h1>hello node class augest node js </h1>")
+})
+
+app.get("/home",(req,res)=>{
+    res.render("Home")
+})
+
+
+app.get("/login",(req,res)=>{
+    res.render("Login")
 })
 
 app.get("/signup",(req,res)=>{
-    res.send("hello node class augest node js" )
+    res.render("Signup")
 })
-app.get("/login",(req,res)=>{
-    res.send("login page")
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
